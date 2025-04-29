73418b9b58048c28e3b32019b00b68cc84389841
Shean-D <sheandivyan01@gmail.com>
2025-04-29 14:32:27 +0530

    add - edit ts

--- src/app/add-edit-employee/add-edit-employee.component.ts
+++ src/app/add-edit-employee/add-edit-employee.component.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { EmployeeService } from '../services/employee.service';
 
 @Component({
   selector: 'app-add-edit-employee',
-  imports: [],
   templateUrl: './add-edit-employee.component.html',
-  styleUrl: './add-edit-employee.component.css'
+  styleUrls: ['./add-edit-employee.component.css'],
 })
-export class AddEditEmployeeComponent {
+export class AddEditEmployeeComponent implements OnInit {
+  employee: any = { name: '', email: '', department: '' };
+  isEditMode = false;
+  errorMessage: string = '';
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private employeeService: EmployeeService
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.isEditMode = true;
+      this.loadEmployee(Number(id));
+    }
+  }
+
+  async loadEmployee(id: number) {
+    try {
+      this.employee = await this.employeeService.getEmployeeById(id);
+    } catch (error) {
+      this.errorMessage = 'Failed to load employee details.';
+    }
+  }
+
+  async onSubmit() {
+    try {
+      if (this.isEditMode) {
+        await this.employeeService.updateEmployee(this.employee.id, this.employee);
+      } else {
+        await this.employeeService.addEmployee(this.employee);
+      }
+      this.router.navigate(['/']);
+    } catch (error) {
+      this.errorMessage = 'Failed to save employee details.';
+    }
+  }
 }
