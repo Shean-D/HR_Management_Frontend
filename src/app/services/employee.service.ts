3bd761af88cd45797e5e03c518b88539b70d1bfc
Shean-D <sheandivyan01@gmail.com>
2025-04-29 14:28:31 +0530

    create a service for API calls

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import axios from 'axios';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EmployeeService {
+  private baseUrl = 'http://localhost:8080/api/employees'; // Backend API URL
 
-  constructor() { }
+  constructor() {}
+
+  
+  async getAllEmployees() {
+    const response = await axios.get(this.baseUrl);
+    return response.data;
+  }
+
+  
+  async addEmployee(employee: any) {
+    const response = await axios.post(this.baseUrl, employee);
+    return response.data;
+  }
+
+  
+  async updateEmployee(id: number, updatedEmployee: any) {
+    const response = await axios.put(`${this.baseUrl}/${id}`, updatedEmployee);
+    return response.data;
+  }
+
+  
+  async deleteEmployee(id: number) {
+    await axios.delete(`${this.baseUrl}/${id}`);
+  }
 }
